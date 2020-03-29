774b96a51cf5128fb9b8abd6cd76386018ed9e97
wajohnson43 <wajohnson4342@gmail.com>
2020-03-29 06:22:03 +0100

    Basic functionality, one pi

---
+++ main.c
@@ -0,0 +1,57 @@
+/*
+ *Written By:		Will Johnson
+ *Date:			3/29/20 (COVID-19 Pandemic)
+ *Note:			Utilizing Basic BJT transistor and PWM to switch the fan between 0V and 5V.
+ *			Must be run as root (PWM)
+ */
+
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const int PWM_pin = 12;
+
+void initFan(void);		//initialize the fan
+void startFan(void);		//start the fan
+void killFan(void);		//kill the fan
+double getTemp(void);		//get the CPU's temperature
+
+int main(void){
+	initFan();
+	double temp;
+	temp = getTemp();
+	if(temp > 40){
+		startFan();
+	}
+	else if(temp <= 40){
+		killFan();
+	}
+}
+
+void initFan(void){
+	if(wiringPiSetupGpio() == -1){
+		exit -1;
+	}
+	pinMode(PWM_pin, PWM_OUTPUT);
+}
+
+void startFan(void){
+	pwmWrite(PWM_pin, 1024);
+}
+
+void  killFan(void){
+	pwmWrite(PWM_pin, 0);
+}
+
+double getTemp(void){
+	FILE *tempFile;
+	double temp;
+	tempFile = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
+	if(tempFile == NULL){
+		printf("Temp FIle not found..."); 
+	}
+	fscanf(tempFile, "%lf", &temp);
+	temp /= 1000;
+	fclose (tempFile);
+	return temp;
+}
